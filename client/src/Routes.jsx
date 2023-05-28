3491b83a00453719366007b9d11207e3a05f2f9a
Tushar agrawal <gargtushar495@gmail.com>
2023-05-29 00:53:23 +0530

    add askQuestion page

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import {Route, Routes} from "react-router-dom";
 import Home from './pages/Home/Home'
 import Auth from './pages/Auth/Auth'
+import Questions from './pages/Questions/Questions';
+import AskQuestion from './pages/AskQuestion/AskQuestion';
 const AllRoutes = () => {
   return (
     <>
       <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route exact path='/Auth' element = {<Auth/>}/>
+        <Route exact path='/Questions' element = {<Questions/>}/>
+        <Route exact path='/AskQuestion' element = {<AskQuestion/>}/>
       </Routes>
     </>
   )
--- client/src/components/HomeMainbar/HomeMainbar.jsx
+++ client/src/components/HomeMainbar/HomeMainbar.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import './HomeMainbar.css'
+import QuestionList from './QuestionList'
 
 const HomeMainbar = () => {
+
+  var questionList = [{
+    id: 1,
+    votes: 2,
+    noOfAnswers: 2,
+    questionTitle: 'What is function',
+    questionTag: ['java', 'node js', 'react js'],
+    userPosted: 'diku',
+    askedOn: 'dec 2'
+  }, {
+    id: 1,
+    votes: 2,
+    noOfAnswers: 2,
+    questionTitle: 'What is function',
+    questionTag: ['java', 'node js', 'react js'],
+    userPosted: 'diku',
+    askedOn: 'dec 2'
+  }, {
+    id: 1,
+    votes: 2,
+    noOfAnswers: 2,
+    questionTitle: 'What is function',
+    questionTag: ['java', 'node js', 'react js'],
+    userPosted: 'diku',
+    askedOn: 'dec 2'
+  }]
+
+  const user = 7;
+  const navigate = useNavigate();
+
+  const checkAuth = () =>{
+    if(user === null){
+      alert("login or signup to ask a question");
+      navigate('/Auth');
+    }else{
+      navigate('/AskQuestion')
+    }
+  }
+  const location = useLocation();
+
   return (
-    <div>
-      
+    <div className='main-bar'>
+      <div className='main-bar-header'>
+        {
+          location.pathname === '/' ? <h1>Top Questions</h1> : <h1>All Questions</h1>
+        }
+        <button onClick={checkAuth} className='ask-btn'>Ask Question</button>
+      </div>
+      <div>
+        {
+          questionList === null ?
+            <h1>Loading...</h1> :
+            <>
+              <p>{questionList.length} questions</p>
+              <QuestionList questionList = {questionList}/>
+            </>
+        }
+      </div>
     </div>
   )
 }
